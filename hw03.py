270642c53ca76117002703802d3faacd6f6dad93
taeyoung <learner1day@gmail.com>
2020-10-21 13:32:26 +0900

    upload homework

--- hw03.py
+++ hw03.py
@@ -1,1 +1,22 @@
-print('I do my homework')
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta    # 'dbsparta'라는 이름의 db를 사용합니다. 'dbsparta' db가 없다면 새로 만듭니다.
+
+# 타겟 URL을 읽어서 HTML를 받아오고,
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713', headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+soup = BeautifulSoup(data.text, 'html.parser')
+# select_one 을 사용해서 차트를 가져온다.
+music_chart = soup.select('#body-content > div.newest-list > div > table > tbody > tr ')
+for music in music_chart:
+    rank = music.select_one('td.number').text.split()[0]
+    music_title = music.select_one('td.info > a.title.ellipsis').text.strip()
+    artist = music.select_one('td.info > a.artist.ellipsis').text
+
+    db.genie_music.insert({'rank':rank,'title':music_title,'artist':artist})
